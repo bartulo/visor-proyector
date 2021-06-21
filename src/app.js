0eee68dd19ada38dd5fa461b810865cb51863be8
bartulo <bartulo@gmail.com>
2021-06-21 13:37:53 +0200

    nuevo escenario, implementacion de cambio de textura

--- src/app.js
+++ src/app.js
@@ -10,13 +10,15 @@ import {
 
 //import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { OrbitControls } from './OrbitControls.js';
-import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer';
+import { CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer';
+import { io } from 'socket.io-client';
 
 import TerrainLoader from './terrain.js';
 import { Sidebar } from './sidebar.js';
 
 import Mdt from './images/mdt.bin';
 import Pnoa from './images/pnoa.jpg';
+import Topo from './images/topo.jpg';
 import './css/sidebar.css';
 import './icons/style.css';
 
@@ -27,11 +29,12 @@ class App {
 
 	init() {
 
+    const socket = io();
     sidebar = new Sidebar();
     sidebar.init();
 
 		camera = new PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
-		camera.position.set( 0, 400, 0 );
+		camera.position.set( 0, 800, 0 );
 
 		scene = new Scene();
 
@@ -58,12 +61,35 @@ class App {
     var terrainLoader = new TerrainLoader();
     terrainLoader.load(Mdt, function(data) {
       console.log( data );
-      const geometry = new PlaneGeometry( 300, 300, 239, 239 );
+      const geometry = new PlaneGeometry( 680, 384, 169, 95 );
       const texture = new TextureLoader().load(Pnoa);
+      const texture2 = new TextureLoader().load(Topo);
       const material = new MeshBasicMaterial({map: texture});
+
+      document.addEventListener('keydown', ( event ) => {
+        
+        if (event.key == 't') {
+
+          if ( material.map == texture ) {
+
+            material.map = texture2;
+            socket.emit( 'tecla', 'topo' );
+            
+          } else {
+
+            material.map = texture;
+            socket.emit( 'tecla', 'pnoa' );
+
+          }
+
+        }
+
+      });
+
       for (let i = 0; i < data.length; i++) {
-        geometry.attributes.position.array[(i*3) + 2] = data[i] / 65535 * 8000;
+        geometry.attributes.position.array[(i*3) + 2] = data[i] / 65535 * 20000;
       }
+
       const plane = new Mesh(geometry, material);
       plane.rotation.set( Math.PI / 2, Math.PI, Math.PI );
       scene.add(plane);
